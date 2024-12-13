e3131ebbdd1de05a7bf99521e120b79cbb54bc1f
Ashutosh D. Pandey <96934931+Ashutoshpandey636@users.noreply.github.com>
2024-12-13 18:01:56 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,13 @@ import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
+import Resume from "../assets/Ashutosh_Pandey_Resume.pdf";
 
 const Navbar = () => {
   return <nav className="mb-20 flex items-center justify-between py-6">
     <div className="flex flex-shrink-0 items-center">
-      <img className="h-10 w10" src={logo} alt="logo"/>
+      <a href={Resume} download="Ashutosh_Pandey_Resume.pdf"><img className="h-10 w10" src={logo} alt="logo"/></a>
+      
     </div>
     <div className="m-8 flex items-center justify-center gap-4 text-2xl">
       <a href="https://www.linkedin.com/in/ashutoshpandey-dev/"><FaLinkedin/></a>
